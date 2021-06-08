f2c836f6a7e2d4858d799a3cffd37cb7b927f4b1
emoacht <emotom@pobox.com>
2021-06-08 18:45:49 +0900

    Add WPF TouchPad example

---
+++ RawInput.Sharp.TouchPadExample/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Linearstar.Windows.RawInput;
+
+namespace RawInput.Sharp.TouchPadExample
+{
+	public partial class MainWindow : Window
+	{
+		public TouchPadManipulation Manipulation { get; } = new TouchPadManipulation(TimeSpan.FromMilliseconds(50));
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private HwndSource _targetSource;
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+
+			_targetSource = (HwndSource)PresentationSource.FromVisual(this);
+			_targetSource.AddHook(WndProc);
+
+			// Register the HidUsageAndPage to watch TouchPad.
+			RawInputDevice.RegisterDevice(HidUsageAndPage.TouchPad, RawInputDeviceFlags.None, _targetSource.Handle);
+		}
+
+		private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			const int WM_INPUT = 0x00FF;
+
+			switch (msg)
+			{
+				case WM_INPUT:
+					try
+					{
+						if (RawInputData.FromHandle(lParam) is RawInputDigitizerData data)
+						{
+							Manipulation.ParseInput(data);
+						}
+					}
+					catch (ArgumentException)
+					{
+						// RawInputDigitizerContact constructor may throw ArgumentException.
+					}
+					break;
+			}
+			return IntPtr.Zero;
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+
+			RawInputDevice.UnregisterDevice(HidUsageAndPage.TouchPad);
+		}
+	}
+}
---
+++ RawInput.Sharp.TouchPadExample/TouchPadManipulation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+using Linearstar.Windows.RawInput;
+
+namespace RawInput.Sharp.TouchPadExample
+{
+	public class TouchPadManipulation : DependencyObject
+	{
+		public Point Current
+		{
+			get { return (Point)GetValue(CurrentProperty); }
+			set { SetValue(CurrentProperty, value); }
+		}
+		public static readonly DependencyProperty CurrentProperty =
+			DependencyProperty.Register("Current", typeof(Point), typeof(TouchPadManipulation), new PropertyMetadata(default(Point)));
+
+		public Vector Delta
+		{
+			get { return (Vector)GetValue(DeltaProperty); }
+			set { SetValue(DeltaProperty, value); }
+		}
+		public static readonly DependencyProperty DeltaProperty =
+			DependencyProperty.Register("Delta", typeof(Vector), typeof(TouchPadManipulation), new PropertyMetadata(default(Vector)));
+
+		private readonly DispatcherTimer _timer;
+
+		public TouchPadManipulation(TimeSpan interval)
+		{
+			_timer = new DispatcherTimer { Interval = interval };
+			_timer.Tick += OnTick;
+		}
+
+		private readonly object _lock = new();
+
+		private Point _origin;
+
+		public void ParseInput(RawInputDigitizerData data)
+		{
+			var contact = data.Contacts[0];
+			if (contact.Identifier == 0) // Use the first contact only.
+			{
+				lock (_lock)
+				{
+					Current = new Point(contact.X, contact.Y);
+
+					if (!_timer.IsEnabled)
+					{
+						_origin = Current;
+					}
+					else
+					{
+						_timer.Stop();
+					}
+					_timer.Start();
+				}
+			}
+		}
+
+		private void OnTick(object sender, EventArgs e)
+		{
+			lock (_lock)
+			{
+				_timer.Stop();
+
+				Delta = new Vector(Current.X - _origin.X, Current.Y - _origin.Y);
+				_origin = default;
+			}
+		}
+	}
+}
